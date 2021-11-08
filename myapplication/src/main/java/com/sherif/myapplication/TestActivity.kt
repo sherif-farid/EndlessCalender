e581cfc21e3178591a1ef73dd0d6626e495386f1
sherf <sherffareed39@gmail.com>
2021-11-08 23:59:36 +0200

    save

--- myapplication/src/main/java/com/sherif/myapplication/TestActivity.kt
+++ myapplication/src/main/java/com/sherif/myapplication/TestActivity.kt
@@ -2,7 +2,6 @@ package com.sherif.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.sherif.myapplication.databinding.ActivityMainBinding
 import com.sherif.mycalender.OnDateSelected
 
